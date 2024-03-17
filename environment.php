f79e4d3077b0c708bcf731e25c3d605d07b5ff9a
iarley <7iarley7@gmail.com>
2024-03-17 14:29:49 -0300

    Feat: Create a Define Paths to the project

--- environment.php
+++ environment.php
@@ -2,6 +2,15 @@
 
 namespace App;
 
-define('BASE_URL', 'http://localhost/PHP/PHP_MVC');
-define('VIEW_URL', __DIR__ . '/views/');
-define('ASSETS', './css/');
+// Urls for project
+define('BASE_URL',  'http://localhost/PHP/Dashboard - php - mysql - bootstrap5/App/');
+define('VIEW_URL',  __DIR__ . '/views/');
+define('ASSETS',    './css/');
+
+// Connection to Database
+define('DRIVER',    'mysql');
+define('DBNAME',    'teste');
+define('HOST',      'localhost');
+define('PORT',      '3306');
+define('USER',      'root');
+define('PASSWORD',  '');
