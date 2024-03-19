910a913ec2b4d1adc7d484772a41d9169747b774
iarley <7iarley7@gmail.com>
2024-03-19 15:17:27 -0300

    Fix: Enviroment vars

--- App/Environment.php
+++ App/Environment.php
@@ -4,5 +4,7 @@ namespace App;
 
 // Urls for project
 define('BASE_URL',  'http://localhost/PHP/Dashboard - php - mysql - bootstrap5/App/');
-define('VIEW_URL',  __DIR__ . '/Resource/Views/');
-define('ASSETS'  ,    './Resource/Css/');
+define('VIEW_URL',  __DIR__ . '\\Resource\\Views\\');
+// com __DIR__ nao pega o caminho do link
+// define('ASSETS', '.\\Resource\\Assets\\');
+define('ASSETS', BASE_URL . '\\Resource\\Assets\\');
