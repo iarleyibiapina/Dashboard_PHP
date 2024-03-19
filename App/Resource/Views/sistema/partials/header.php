6f3d5127f4ae4d701ef9a7569db19ccfb087cf3f
iarley <7iarley7@gmail.com>
2024-03-19 18:05:37 -0300

    Feat: add the favicon

--- App/Resource/Views/sistema/partials/header.php
+++ App/Resource/Views/sistema/partials/header.php
@@ -10,8 +10,10 @@
     <title><?= $title ?></title>
     <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
     <link href="<?= ASSETS ?>Css/styles.css" rel="stylesheet" />
-    <!-- <link href="Resource/Assets/Css/styles.css" rel="stylesheet" /> -->
     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+    <link type="image/png" sizes="16x16" rel="icon" href="<?= ASSETS ?>/img/sprint-report.ico">
+    <link type="image/png" sizes="32x32" rel="icon" href="<?= ASSETS ?>/img/sprint-report.ico">
+    <link type="image/png" sizes="96x96" rel="icon" href="<?= ASSETS ?>/img/sprint-report.ico">
 </head>
 
 <body class="sb-nav-fixed">
